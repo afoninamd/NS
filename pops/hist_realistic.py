Correct GR calculation

pops/hist_realistic.py
@@ -25,10 +25,11 @@ from main.constants import arr_size, galaxy_age, Gyr, Myr, year
 # full_path = '/home/afoninamd/Downloads/all_feathers/'
 # # full_path = '/home/afoninamd/Downloads/realistic2/'
 # path = '/home/afoninamd/Downloads/'
-full_path = '/home/afoninamd/Documents/NS/project/pops/result/realistic/'
+# full_path = '/home/afoninamd/Documents/NS/project/pops/result/realistic/'
+full_path = '/home/afoninamd/Documents/NS/project/pops/result/together/'
 # res_name = 'realistic/'
 # path = '/home/afoninamd/Documents/project/pops/result/realistic/'
-# res_name = ''
+res_name = ''
 Rbins = np.linspace(0, 20, arr_size+1) # from the GC
 rbins = np.linspace(0, 20, arr_size+1) # from the Sun better for 0 to 20
 zbins = np.linspace(0, 5, arr_size+1)
@@ -38,7 +39,7 @@ vbins = np.linspace(0, 500, arr_size+1) * 1e5
 Tbins = 10**np.linspace(5, 9, arr_size+1) # better from 5 to 8
 tbins = np.linspace(0, galaxy_age, arr_size+1)
 Bbins = 10**np.linspace(8, 15, arr_size+1) # magnetic field values
-Mbins = 10**np.linspace(2, 10, arr_size+1)# accretion rates in g/s
+Mbins = 10**np.linspace(2, 15, arr_size+1)# accretion rates in g/s old: from 2 to 10
 Pbins = 10**np.linspace(0, 8, arr_size+1) # spin periods for the P-A transition
 
 """ Bins for the 2d histogram """
@@ -80,10 +81,10 @@ def pdfs():
                 for add_string in ['', '_roman']:
                     file_name = '{}_{}_{}_{}_erosita{}'.format(galaxy_type, field,
                                                     case, 'pulsar', add_string)
-                    df = pd.read_feather(path + res_name + file_name + '.feather')
+                    df = pd.read_feather(full_path + file_name + '.feather')
                     file_name = '{}_{}_{}_{}_erosita{}'.format(galaxy_type, field,
                                                     case, 'magnetar', add_string)
-                    data = df+pd.read_feather(path + res_name + file_name + '.feather')
+                    data = df+pd.read_feather(full_path + file_name + '.feather')
                     
                     arr = cbins
                     data_c1 = np.array(data['c1'])
@@ -176,19 +177,17 @@ def pdfs1():
                 fig, ax = plt.subplots()
                 ax.vlines(x=1e-2, ymin=1e-2, ymax=1e7, color='black', alpha=0.5, ls='--')
                 
-                for add_string in ['', '_roman']:
+                for add_string in ['']:#, '_roman']:
                     file_name = '{}_{}_{}_erosita{}_std'.format(galaxy_type, field,
                                                     case, add_string)
                     
-                    print(path + res_name + file_name + '.feather')
-                    
-                    data_std = pd.read_feather(path + res_name + file_name + '.feather')
+                    data_std = pd.read_feather(full_path + file_name + '.feather')
                     data_c1_std = data_std['c1']
                     std_c1 = data_c1_std #np.cumsum(data_c1_std[::-1])[::-1]
                     
                     file_name = '{}_{}_{}_erosita{}_sum'.format(galaxy_type, field,
                                                     case, add_string)
-                    data = pd.read_feather(path + res_name + file_name + '.feather')
+                    data = pd.read_feather(full_path + file_name + '.feather')
                     
                     arr = cbins
                     data_c1 = np.array(data['c1'])
@@ -231,7 +230,7 @@ def pdfs1():
                 # print(len(std_c1[std_c1!=0]))
                 fig.savefig('counts/counts_{}_{}_{}.pdf'.format(galaxy_type, field, case), format='pdf')
 
-
+# pdfs1()
 
 def plotr():
     for galaxy_type in ['simple','two_phase']:#, 'two_phase']:
@@ -273,7 +272,7 @@ def plotr():
                     # std_2 = np.array(data['r-2'])
                     # std_1 = np.array(data['r-1'])
                     # std_0 = np.array(data['r'])
-                    print(data.columns)
+                    # print(data.columns)
                     std_2 = np.array(data['t-2'])
                     std_1 = np.array(data['t-1'])
                     std_0 = np.array(data['t'])
@@ -317,12 +316,12 @@ def rebin(nrebin=40): # new number of bins
                 case = cases[k]
                 file_name = '{}_{}_{}_erosita{}_std'.format(galaxy_type, field,
                                                 case, '')
-                data_std = pd.read_feather(path + res_name + file_name + '.feather')
+                data_std = pd.read_feather(full_path + file_name + '.feather')
                 data_c1_std = np.array(data_std['c1'])
                 std_c1 = data_c1_std #np.cumsum(data_c1_std[::-1])[::-1]
                 file_name = '{}_{}_{}_erosita{}_sum'.format(galaxy_type, field,
                                                 case, '')
-                data = pd.read_feather(path + res_name + file_name + '.feather')
+                data = pd.read_feather(full_path + file_name + '.feather')
                 
                 bins = cbins #arr[1:]
                 
@@ -358,8 +357,6 @@ def rebin(nrebin=40): # new number of bins
                     # print(norm*data_c1[19], bins[19])
                     ax.plot(bins[15:], norm*diagonal[15:], ls='--', color='grey')
                     
-             
-                print(bins[7])
                 
                 def format_num(n):
                     # numbers < 100 -> 1 significant digit
@@ -381,7 +378,7 @@ def rebin(nrebin=40): # new number of bins
                 ax.set_yscale('log')
                 ax.set_xscale('log')
                 ax.set_xlim([1e-3, 1e1])
-                ax.set_ylim([1e-1, 1e5])
+                ax.set_ylim([3e+0, 2e5])
             ax.legend(title=r'Propeller model')
         for line in lines:
             print(line + ' \\\\')
@@ -518,7 +515,8 @@ def plotrrebin(nrebin=arr_size,field='CF',  case='A', galaxy_type='simple'): # n
                 
                 print('{}_{}_{} T of maximum: {:.3e}'.format(galaxy_type, field, case, x_at_max))
                       
-            
+            if name == 'P' or name == 'M' or name == 'B':
+                ax.set_xscale('log')
             # if cts_num == '-2':
             #     print(case, field, x_99)
             
@@ -558,33 +556,48 @@ def plotrrebin(nrebin=arr_size,field='CF',  case='A', galaxy_type='simple'): # n
 
 def MdotMap(galaxy_type='two_phase', field='ED', case='B'):
     name2d = '{}_{}_{}'.format(galaxy_type, field, case)
+    output_dir = '/home/afoninamd/Documents/NS/project/pops/result/together/'
     number_file = output_dir + name2d + '_Number_Rz.npy'
     mdot_file   = output_dir + name2d + '_Mdot_Rz.npy'
     
-    Mdot_Rz = np.load(mdot_file)
-    Number_Rz = np.load(number_file)
+    Mdot_Rz = (np.load(mdot_file))
+    Number_Rz = (np.load(number_file))
     # print(Mdot_Rz[Mdot_Rz!=0])
     # print(Mdot_Rz[Number_Rz!=0])
     
-    plt.figure(figsize=(7,5))
+    plt.figure(figsize=(8,6))
     
-    plt.pcolormesh(R2bins, z2bins, Number_Rz.T, shading='auto') # Number_Rz also an interesting thing
-    plt.xlabel('R')
-    plt.ylabel('z')
-    plt.title('Mdot(R,z)')
+    counts = np.log10(Mdot_Rz.T/Number_Rz.T)
+    # plt.pcolormesh(R2bins, z2bins, Number_Rz.T, shading='auto') # Number_Rz also an interesting thing
+    cms = plt.pcolormesh(R2bins, z2bins, counts, shading='auto') # Number_Rz also an interesting thing
+    plt.xlabel('$R$, kpc')
+    plt.ylabel('$z$, kpc')
+    # plt.title('Mdot(R,z), [g/s]')
     
     cbar = plt.colorbar()
-    cbar.set_label('Mdot')
+    cbar.set_label('log$_{10}\dot{M}$, [g/s]')
+    
+    # smoothed_counts = sp.ndimage.gaussian_filter(counts, sigma=1)
+    # levels = np.linspace(8,15, 20)
+    # cnt = plt.contour((R2bins[1:]+R2bins[:1])/2, (z2bins[1:]+z2bins[:1])/2, smoothed_counts, levels, colors='white', rasterized=True, linewidths=0.5)
+    #                     # vmin=vmin, vmax=vmax)
+    # mpl.rcParams["text.usetex"] = False
+    # try:
+    #     plt.clabel(cnt, levels, fontsize=10, fmt='%r')
+    # except:
+    #     pass
     
     plt.tight_layout()
     plt.show()
 
+# MdotMap
 # MdotMap(galaxy_type='two_phase', field='ED', case='B')
-# MdotMap(galaxy_type='simple', field='CF', case='A')
+MdotMap(galaxy_type='simple', field='CF', case='A')
 
 """ HERE!!!! """
 # plotr()
-plotrrebin(case='A', field='CF', galaxy_type='simple')
+# plotrrebin(case='A', field='CF', galaxy_type='simple')
+# plotrrebin(case='B', field='ED', galaxy_type='two_phase')
 
 
 
@@ -730,7 +743,7 @@ def table_stages_old():
 
 
 def table_stages():
-    df = pd.read_csv('/home/afoninamd/Documents/NS/project/pops/result/all.txt', sep='\s+', header=None)
+    df = pd.read_csv(full_path+'all.txt', sep='\s+', header=None)
     df_p = df[:16]
     df_m = np.zeros((10,16))
     # df_pr = df[16:32]

pops/observability.py
@@ -104,9 +104,9 @@ def column_density(pos) -> float:
 
 
 def plank(nu, T) -> np.array:
-    r_s = 2 * G * M_NS / c**2
-    g_00 = 1 - r_s/R_NS
-    nu = nu / (g_00)**0.5
+    # r_s = 2 * G * M_NS / c**2
+    # g_00 = 1 - r_s/R_NS
+    # nu = nu / (g_00)**0.5  # that is of no use, we have shifted the tempeature
     B = 2*h*nu**3/c**2 / (np.exp(h*nu/(k_B*T))-1)
     return B
 
@@ -193,7 +193,7 @@ def one_observability(t1, stages1, x1, y1, z1, B1, Mdot1,
     R_A = (mu**2 / (2 * Mdot2 * (2 * G * M_NS)**0.5))**(2/7)
     R_cap = R_NS * (R_NS/R_A)**0.5
     S_cap = 2 * np.pi * R_cap**2
-    Tt = (L_array / S_cap / sigma_B)**0.25
+    Tt = (L_array / g_00**0.5 / S_cap / sigma_B)**0.25 * g_00**0.5
     
     for i in range(lenga):
         N_H = column_density(np.array([x2[i], y2[i], z2[i]]))

pops/pops_mpi_realistic.py
@@ -76,7 +76,7 @@ vbins = np.linspace(0, 500, arr_size+1) * 1e5
 Tbins = 10**np.linspace(5, 9, arr_size+1) # better from 5 to 8
 tbins = np.linspace(0, galaxy_age, arr_size+1)
 Bbins = 10**np.linspace(8, 15, arr_size+1) # magnetic field values
-Mbins = 10**np.linspace(2, 10, arr_size+1)# accretion rates in g/s
+Mbins = 10**np.linspace(2, 15, arr_size+1)# accretion rates in g/s
 Pbins = 10**np.linspace(0, 8, arr_size+1) # spin periods for the P-A transition
 
 """ Bins for the 2d histogram """
@@ -185,7 +185,7 @@ def calculations(star_type):
     for galaxy_type in ['simple', 'two_phase']:
         for field in ['CF', 'ED']:
             for case in ['A', 'B', 'C', 'D']:
-                """ Two-d map for Mdot """
+                """ Creating two-d map for Mdot, B, v """
                 name2d = 'npy/{}_{}_{}_{}_{}'.format(crank, galaxy_type, field,
                                                 case, star_type)
                 nR = len(R2bins) - 1
@@ -193,13 +193,15 @@ def calculations(star_type):
                 shape = (nR, nz)
             
                 number_file = output_dir + name2d + '_Number_Rz.npy'
-                mdot_file   = output_dir + name2d + '_Mdot_Rz.npy'
-                
-                if not os.path.exists(number_file):
-                    np.save(number_file, np.zeros(shape, dtype='float32'))
-                
-                if not os.path.exists(mdot_file):
-                    np.save(mdot_file, np.zeros(shape, dtype='float32'))
+                for name_for_npy in ['_Number_Rz1', '_Number_Rz2',
+                                     '_Mdot_Rz', '_B_Rz', '_v_Rz']:
+                    mdot_file   = output_dir + name2d + name_for_npy + '.npy'
+                    
+                    if not os.path.exists(number_file):
+                        np.save(number_file, np.zeros(shape, dtype='float32'))
+                    
+                    if not os.path.exists(mdot_file):
+                        np.save(mdot_file, np.zeros(shape, dtype='float32'))
     
     """ Starting reading the files """
     for i in (range(start_idx, end_idx)): #tqdm
@@ -339,12 +341,25 @@ def calculations(star_type):
                         M1counts, _ = np.histogram(Mtemp, bins=Mbins, weights=weight1)
                         
                         """ Two-d histogram for the NS position """
-                        Number, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=accretor_weight)
-                        """ Two-d histogram for M_dot """
+                        Number_Rz, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=accretor_weight)
+                        Number_Rz1, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=weight1)
+                        Number_Rz2, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=weight2)
+                        """ Two-d histogram for M_dot, B, v """
                         Mtemp_accretor = np.zeros(len(Mtemp))
                         Mtemp_accretor[::] = Mtemp[::]
                         Mtemp_accretor[stages1[1:]!=3] = 0
                         Mdot_Rz, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=Mtemp_accretor)
+                        
+                        Btemp_accretor = np.zeros(len(Btemp))
+                        Btemp_accretor[::] = Btemp[::]
+                        Btemp_accretor[stages1[1:]!=3] = 0
+                        B_Rz, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=Btemp_accretor)
+                        
+                        vtemp_accretor = np.zeros(len(vtemp))
+                        vtemp_accretor[::] = vtemp[::]
+                        vtemp_accretor[stages1[1:]!=3] = 0
+                        v_Rz, _, _ = np.histogram2d(R, ztemp, bins=[R2bins, z2bins], weights=vtemp_accretor)
+                        
                         
                         f0counts, _ = np.histogram(f0[1:], bins=fbins, weights=weight)
                         f1counts, _ = np.histogram(f1[1:], bins=fbins, weights=weight)
@@ -370,22 +385,41 @@ def calculations(star_type):
                         df0 = pd.read_feather(output_dir + name + '.feather')
                         feather.write_feather(df+df0, output_dir + name + '.feather')
                         
+                        
                         """ two-d files """
                         name2d = 'npy/{}_{}_{}_{}_{}'.format(crank, galaxy_type, field,
                                                         case, star_type)
                         number_file = output_dir + name2d + '_Number_Rz.npy'
-                        mdot_file   = output_dir + name2d + '_Mdot_Rz.npy'
-                        
-                        
                         Number_old = np.load(number_file)
-                        Number_new = Number_old + Number
+                        Number_new = Number_old + Number_Rz
                         np.save(number_file, Number_new)
                         
+                        number_file = output_dir + name2d + '_Number_Rz1.npy'
+                        Number_old = np.load(number_file)
+                        Number_new = Number_old + Number_Rz1
+                        np.save(number_file, Number_new)
+                        
+                        number_file = output_dir + name2d + '_Number_Rz2.npy'
+                        Number_old = np.load(number_file)
+                        Number_new = Number_old + Number_Rz2
+                        np.save(number_file, Number_new)
+                        
+                        mdot_file = output_dir + name2d + '_Mdot_Rz.npy'
                         Mdot_old = np.load(mdot_file)
                         Mdot_new = Mdot_old + Mdot_Rz
                         np.save(mdot_file, Mdot_new)
                         
-                        # """ For Erosita + Roman """
+                        B_file = output_dir + name2d + '_B_Rz.npy'
+                        B_old = np.load(B_file)
+                        B_new = B_old + B_Rz
+                        np.save(B_file, B_new)
+                        
+                        v_file = output_dir + name2d + '_v_Rz.npy'
+                        v_old = np.load(v_file)
+                        v_new = v_old + v_Rz
+                        np.save(v_file, v_new)
+                        
+                        """ For Erosita + Roman """
                         # roman = np.zeros(len(x1))
                         # roman[1:][cond] = 1
                         # weight = weight * roman[1:]

pops/put_together.py
@@ -288,48 +288,49 @@ def create_npy():
         for field in ['CF', 'ED']:
             for case in ['A', 'B', 'C', 'D']:
                 
-                pattern = f"{output_dir}/npy/*{galaxy_type}_{field}_{case}*Number_Rz.npy"
+                for count_num in ['', '1', '2']:
+                    pattern = f"{output_dir}/npy/*{galaxy_type}_{field}_{case}*Number_Rz{count_num}.npy"
             
-                files = glob.glob(pattern)
+                    files = glob.glob(pattern)
+                    
+                    Number_sum = None
+                    
+                    for file in files:
+                        data = np.load(file)
+                        # print(data[data!=0])
+                        if Number_sum is None:
+                            Number_sum = np.zeros_like(data)
+                    
+                        Number_sum += data
+                    
+                    # print("Loaded", len(files), "cranks")
+                    
+                    Number_Rz = Number_sum
+                    
+                    name2d = '{}_{}_{}'.format(galaxy_type, field, case)
+                    number_file = output_dir + name2d + '_Number_Rz{}.npy'.format(count_num)
+                    
+                    np.save(number_file, Number_Rz)
                 
-                Number_sum = None
                 
-                for file in files:
-                    data = np.load(file)
-                    # print(data[data!=0])
-                    if Number_sum is None:
-                        Number_sum = np.zeros_like(data)
+                for var_name in ['_Mdot_Rz', '_v_Rz', '_B_Rz']:
+                    pattern = f"{output_dir}/npy/*{galaxy_type}_{field}_{case}*{var_name}.npy"
                 
-                    Number_sum += data
-                
-                # print("Loaded", len(files), "cranks")
-                
-                Number_Rz = Number_sum
-                
-                name2d = '{}_{}_{}'.format(galaxy_type, field, case)
-                number_file = output_dir + name2d + '_Number_Rz.npy'
-                mdot_file   = output_dir + name2d + '_Mdot_Rz.npy'
-                
-                np.save(number_file, Number_Rz)
-                
-                
-                pattern = f"{output_dir}/npy/*{galaxy_type}_{field}_{case}*Mdot_Rz.npy"
-            
-                files = glob.glob(pattern)
-                
-                Number_sum = None
-                
-                for file in files:
-                    data = np.load(file)
-                    if Number_sum is None:
-                        Number_sum = np.zeros_like(data)
-                
-                    Number_sum += data
-                
-                # print("Loaded", len(files), "cranks")
-                
-                Mdot_Rz = Number_sum
-                np.save(mdot_file, Mdot_Rz)
+                    files = glob.glob(pattern)
+                    
+                    Number_sum = None
+                    
+                    for file in files:
+                        data = np.load(file)
+                        if Number_sum is None:
+                            Number_sum = np.zeros_like(data)
+                    
+                        Number_sum += data
+                    
+                    # print("Loaded", len(files), "cranks")
+                    mdot_file   = output_dir + name2d + '{}.npy'.format(var_name)
+                    Mdot_Rz = Number_sum
+                    np.save(mdot_file, Mdot_Rz)
     
 
 print("create txt...")